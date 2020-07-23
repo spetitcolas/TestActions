628ddbd826b0abcb85ac3e0a98836abbc761273d
spetitcolas <54885792+spetitcolas@users.noreply.github.com>
2020-07-23 15:24:40 +0200

    Commit

--- ConsoleApp1/ConsoleAppTest/My Project/AssemblyInfo.vb
+++ ConsoleApp1/ConsoleAppTest/My Project/AssemblyInfo.vb
@@ -6,7 +6,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("ConsoleAppTest")>
-<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyCopyright("Copyright ©  2020)")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -14,5 +14,5 @@ Imports System.Runtime.InteropServices
 <Assembly: Guid("6a5c224b-ae83-45e4-a5a7-7a6e9b3a919d")>
 
 ' <Assembly: AssemblyVersion("1.0.*")> 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("9.9.9.9")>
+<Assembly: AssemblyFileVersion("9.9.9.9")>
